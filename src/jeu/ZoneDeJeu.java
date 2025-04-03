a81f0b69dede7ff9e07f45b3451b127cbec6baef
Cédric Roussel <cedricroussel2002@gmail.com>
2025-04-03 09:06:05 +0200

    TP3
    Partie 3

--- src/jeu/ZoneDeJeu.java
+++ src/jeu/ZoneDeJeu.java
@@ -8,11 +8,15 @@ import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
 
+import cartes.Attaque;
 import cartes.Bataille;
 import cartes.Borne;
 import cartes.Carte;
+import cartes.Cartes;
+import cartes.DebutLimite;
 import cartes.FinLimite;
 import cartes.Limite;
+import cartes.Parade;
 
 public class ZoneDeJeu {
 	private List<Limite> pileLimite;
@@ -25,6 +29,10 @@ public class ZoneDeJeu {
 		collectionBorne = new HashSet<>();
 	}
 	
+	/**
+	 * 
+	 * @return la limite de vitesse en cours du joueur en int 
+	 */
 	public int donnerLimitationVitesse() {
 		if (pileLimite.isEmpty() || pileLimite.getFirst() instanceof FinLimite)
 			return 200;
@@ -32,6 +40,10 @@ public class ZoneDeJeu {
 			return 50;
 	}
 	
+	/**
+	 * 
+	 * @return le nombre de km parcourus par le joueur en int
+	 */
 	public int donnerKmParcourus() {
 		int km = 0;
 		for (Borne borne : collectionBorne) {
@@ -40,13 +52,81 @@ public class ZoneDeJeu {
 		return km;
 	}
 	
+	/**
+	 *
+	 * @param c de type Borne, Limite ou Bataille
+	 */
 	public void deposer(Carte c) {
-		if (c instanceof Borne borne)
-			collectionBorne.add(borne);
-		if (c instanceof Limite limite)
-			pileLimite.add(limite);
-		if (c instanceof Bataille bataille)
-			pileBataille.add(bataille);
+		switch (c) {
+			case Borne borne: collectionBorne.add(borne); break;
+			case Limite limite: pileLimite.addFirst(limite); break;
+			case Bataille bataille: pileBataille.addFirst(bataille); break;
+			default:
+				break;
+		}
+	}
+	
+	/**
+	 * 
+	 * @return true si le joueur peut avancer
+	 */
+	public boolean peutAvancer() {
+		return !pileBataille.isEmpty() && 
+				pileBataille.getFirst().equals(Cartes.FEU_VERT);
+	}
+	
+	private boolean estDepotFeuVertAutorise() {
+		return 	pileBataille.isEmpty() ||
+				pileBataille.getFirst().equals(Cartes.FEU_ROUGE) ||
+				!pileBataille.getFirst().equals(Cartes.FEU_VERT);
+	}
+	
+	private boolean estDepotBorneAutorise(Borne borne) {
+		return 	peutAvancer() &&
+				borne.getKm() <= donnerLimitationVitesse() &&
+				donnerKmParcourus() + borne.getKm() <= 1000;
+	}
+	
+	private boolean estDepotLimiteAutorise(Limite limite) {
+		if (limite instanceof DebutLimite)
+			return pileLimite.isEmpty() || pileLimite.getFirst() instanceof FinLimite;
+		else
+			return pileLimite.getFirst() instanceof DebutLimite;
+	}
+	
+	private boolean estDepotBatailleAutorise(Bataille bataille) {
+		if (bataille instanceof Attaque) return peutAvancer();
+		else if (bataille instanceof Parade) {
+			if (bataille.equals(Cartes.FEU_VERT)){
+				return 	estDepotFeuVertAutorise();
+			} else { 
+				System.out.println(pileBataille.getFirst().getType());
+				System.out.println(bataille.getType());
+				return !pileBataille.isEmpty() &&
+					pileBataille.getFirst() instanceof Attaque &&
+					pileBataille.getFirst().getType().equals(bataille.getType());	
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param carte de type Borne, Limite ou Bataille
+	 * @return true si la carte peut être déposée
+	 */
+	public boolean estDepotAutorise(Carte carte) {
+//		System.out.println("Affichage zone de jeu avant depot");
+//		System.out.println("Limite : " + pileLimite);
+//		System.out.println("Borne : " + collectionBorne);
+//		System.out.println("Bataille : " + pileBataille);
+		switch (carte) {
+		case Borne borne : return estDepotBorneAutorise(borne);
+		case Limite limite : return estDepotLimiteAutorise(limite);
+		case Bataille bataille : return estDepotBatailleAutorise(bataille);
+		
+		default : return false;
+		}
 	}
 
 	
